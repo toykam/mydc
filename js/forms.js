671c9da0ae1a665cc1c875c4cbdf8b166c866a55
= <=>
2019-11-10 18:12:58 +0100

    Added Forms.js File

---
+++ js/forms.js
@@ -0,0 +1,35 @@
+$('form').submit(function (e) {
+    e.preventDefault();
+    const msg = $(this).attr('msg');
+    const action = $(this).attr('action');
+    const method = $(this).attr('method');
+    const data = $(this).serializeArray();
+
+    const msgDisplay = $('#msg');
+    const msgDisplayText = $('#msgText');
+    const msgDisplayIcon = $('#msgIcon');
+
+    msgDisplayText.text(msg);
+    msgDisplayIcon.addClass('fa-spinner fa-spin');
+    msgDisplay.show();
+    // alert(JSON.stringify(data));
+    $.ajax({
+      url: action,
+      type: method,
+      data: data,
+      dataType: 'JSON',
+      success: (resData) => {
+        console.log(resData);
+        if (resData.status == 1) {
+          msgDisplayIcon.removeClass('fa-spinner fa-spin').addClass('fa-check-circle');
+        } else {
+          msgDisplayIcon.removeClass('fa-spinner fa-spin').addClass('fa-exclamation-triangle');
+        }
+        msgDisplay.addClass(resData.flag);
+        msgDisplayText.text(resData.msg);
+        // console.log(resData);
+      }, error: (error) => {
+        console.log(error);
+      }
+    });
+  });
