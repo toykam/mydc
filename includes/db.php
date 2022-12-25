24180cb216019808dd5f4aa501a5ae6e06423f83
toykam <kabirtoyyib19@gmail.com>
2022-12-25 08:13:06 +0100

    testing sss

--- includes/db.php
+++ includes/db.php
@@ -2,20 +2,20 @@
 
 use Medoo\Medoo;
 
-$db = new Medoo([
-    'database_type' => 'mysql',
-    'database_name' => 'mydc',
-    'server' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-]);
-
 // $db = new Medoo([
 //     'database_type' => 'mysql',
-//     'database_name' => 'mydc_mydc',
+//     'database_name' => 'mydc',
 //     'server' => 'localhost',
-//     'username' => 'mydc_mydc',
-//     'password' => '(s8pnhLCIuja',
+//     'username' => 'root',
+//     'password' => '',
 // ]);
+
+$db = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'mydc_mydc',
+    'server' => 'localhost',
+    'username' => 'mydc_mydc',
+    'password' => '(s8pnhLCIuja',
+]);
 
 ?>
